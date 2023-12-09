933e18f7ee9c50fc9ba165e2372ba3a81f043ccc
William Danton <Dwilliamanton@yahoo.com>
2023-12-09 14:28:01 -0500

    Class 31

---
+++ class31/class31-materials/objects-child-class/js/main.js
@@ -0,0 +1,18 @@
+//Create an a class and extend it - Can be anything you would like it to be! 
+class animal = {
+    this.name; name
+    this.breed; breed
+}
+class dog extends animal {
+    constructor(name,breed){
+        super(name)
+        super(breed) = this.breed
+    }
+}
+
+class ferret extends animal {
+    constructor(name,breed){
+        super(name) =  this.name
+        super(breed) = this.breed
+    }
+}
