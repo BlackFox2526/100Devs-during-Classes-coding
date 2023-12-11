a84482852b2b1f3eb9c6fa11678f258acab33442
William Danton <Dwilliamanton@yahoo.com>
2023-12-11 18:30:27 -0500

    Completed class 32

---
+++ class32/objects-practice/js/main.js
@@ -0,0 +1,82 @@
+// Come up with with a parent class
+// Extend that parent class into two children
+// Use Encapsulation, Abstraction, Inheritance, and Polymorphism 
+
+class Contractor{
+    constructor(name,role){
+        this._name = name
+        this._role = role
+    }
+    get name(){
+        return this._name
+    }
+    get role(){
+        return this._role
+    }
+    sayHello(){
+        console.log(`Hello, my name is ${this._name} and I am the ${this._role} on the team.`)
+    }
+}
+
+class Team extend Contractor{
+    constructor(name, role){
+        super(name,role)
+        this._tech = _tech
+        get tech(){
+            return this._tech
+        }
+        sayHello(){
+            console.log(`Hello, my name is ${this._name} and I am the ${this._role} on the team and I use ${this._tech}`)
+        }
+    }
+}
+let Micheal = new contractor('Micheal', 'Front-end')
+let Rob = new Team('Rob', 'Front-end', 'React')
+class Contractor{
+    constructor(name,role){
+        this._name = name
+        this._role = role
+    }
+    get name(){
+        return this._name
+    }
+    get role(){
+        return this._role
+    }
+    sayHello(){
+        console.log(`Hello, I am on the ${this._role} team at #100Devs!`)
+    }
+}
+class Front extends Contractor{
+    constructor(name,role,tech){
+        super(name,role)
+        this._tech = tech
+    }
+    get tech(){
+        return this._tech
+    }
+    sayHello(){
+        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+    }
+}
+class Back extends Contractor{
+    constructor(name,role,tech){
+        super(name,role)
+        this._tech = tech
+    }
+    get tech(){
+        return this._tech
+    }
+    sayHello(){
+        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+    }
+}
+let bob = new Contractor('Bob','Front-end')
+let simba = new Front('Simba','Front-end','React')
+let machi = new Back('The Machine','Back-end','Node')
+
+let agency = [bob,simba,machi]
+
+for(person of agency){
+    person.sayHello()
+}
